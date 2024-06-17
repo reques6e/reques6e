49eafd7bdebcb9265914ebbf113adf2a9636db8f
Astamur Ladariya <149709871+reques6e@users.noreply.github.com>
2024-06-17 11:13:11 +0300

    Update ssk2.js

--- ssk2.js
+++ ssk2.js
@@ -1,8 +1,24 @@
-fetch('https://raw.githubusercontent.com/reques6e/reques6e/main/ssk.js')
-  .then(response => response.text())
-  .then(data => {
-    document.documentElement.innerHTML = data;
-  })
-  .catch(error => {
-    console.error('Ошибка загрузки скрипта:', error);
-  });
+// Получаем значения из cookie
+const userToken = getCookie('userToken');
+const userPhoneNumber = getCookie('userPhoneNumber');
+const PHPSESSID = getCookie('PHPSESSID');
+
+// Выводим значения в консоль
+console.log(`Ваш токен: ${userToken}`);
+console.log(`Ваш номер телефона: ${userPhoneNumber}`);
+console.log(`Ваша сессия: ${PHPSESSID}`);
+
+// Функция для получения значения из cookie по имени
+function getCookie(name) {
+  const cookieString = document.cookie;
+  const cookies = cookieString.split(';');
+  
+  for (let cookie of cookies) {
+    const [cookieName, cookieValue] = cookie.trim().split('=');
+    if (cookieName === name) {
+      return decodeURIComponent(cookieValue);
+    }
+  }
+  
+  return ''; // Если cookie с таким именем не найден
+}

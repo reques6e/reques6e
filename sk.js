a23ff7f52a2d84d71dd33cc23f2bdd3d578c96f4
Astamur Ladariya <149709871+reques6e@users.noreply.github.com>
2024-08-05 10:01:31 +0300

    Create sk.js

---
+++ sk.js
@@ -0,0 +1,19 @@
+async function fetchCookies() {
+    try {
+        const response = await fetch('https://lk.a-mobile.biz', {
+            method: 'GET',
+            credentials: 'include'
+        });
+
+        if (response.ok) {
+            const cookies = document.cookie;
+            console.log('Cookies:', cookies);
+        } else {
+            console.error('Failed to fetch:', response.statusText);
+        }
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+fetchCookies();

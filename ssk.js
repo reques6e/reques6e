3d834ec2ba329477fbaa8775108265eed71f9dfd
Astamur Ladariya <149709871+reques6e@users.noreply.github.com>
2024-10-14 21:01:32 +0300

    Update ssk.js

--- ssk.js
+++ ssk.js
@@ -1,10 +1,1 @@
-document.documentElement.innerHTML = `
-<html data-lt-installed="true">
-<head>
-    <meta name="color-scheme" content="light dark">
-</head>
-<body>
-<pre style="word-wrap: break-word; white-space: pre-wrap;">Reques6e...
-</pre>
-</body>
-</html>`;
+0
